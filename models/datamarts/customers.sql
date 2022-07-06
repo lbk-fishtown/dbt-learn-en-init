c4496300776dc61463d543e1748fa87f530e1167
lbk-fishtown <79948589+lbk-fishtown@users.noreply.github.com>
2022-07-06 10:19:04 +1000

    Update and rename models/customers.sql to models/datamarts/customers.sql

--- models/datamarts/customers.sql
+++ models/datamarts/customers.sql
@@ -5,7 +5,7 @@ with customers as (
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from raw.jaffleshop.customers
 
 ),
 
@@ -17,7 +17,7 @@ orders as (
         order_date,
         status
 
-    from raw.jaffle_shop.orders
+    from raw.jaffleshop.orders
 
 ),
 
