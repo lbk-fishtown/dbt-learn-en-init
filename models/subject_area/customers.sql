d141c125e2e9c87ab38ca1a6fc04f1edeb1f51e6
Lee Bond-Kennedy <lee.bond.kennedy@fishtownanalytics.com>
2022-07-06 03:02:01 +0000

    build a customers pipeline

--- models/subject_area/customers.sql
+++ models/subject_area/customers.sql
@@ -1,28 +1,18 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
 with customers as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffleshop.customers
-
+    select * from {{ ref('stg_customers') }}
 ),
 
 orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffleshop.orders
-
+    select * from {{ ref('stg_orders') }}
 ),
 
 customer_orders as (
-
     select
         customer_id,
 
